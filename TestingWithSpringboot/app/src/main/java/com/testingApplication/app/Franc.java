43db2a41d02103915a03f51ca43e533e0de9f653
ajay1236671 <kadamajay100@gmail.com>
2025-10-24 12:52:25 +0530

    adding franc

---
+++ TestingWithSpringboot/app/src/main/java/com/testingApplication/app/Franc.java
@@ -0,0 +1,18 @@
+package com.testingApplication.app;
+
+public class Franc {
+    private   int amount;
+
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+
+    Franc times(int  multiplier) {
+        return new Franc(this.amount * multiplier);
+    }
+
+    public boolean equals(Object object) {
+        Franc franc = (Franc) object;
+        return amount == franc.amount;
+    }
+}
