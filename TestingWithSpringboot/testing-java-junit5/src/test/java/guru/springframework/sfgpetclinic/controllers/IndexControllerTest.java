da6665e6106c652b610ed08402e9b53bafb05246
ajay1236671 <kadamajay100@gmail.com>
2025-10-28 14:34:19 +0530

    conditional Testing

--- TestingWithSpringboot/testing-java-junit5/src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ TestingWithSpringboot/testing-java-junit5/src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
 
 import java.time.Duration;
 
@@ -55,5 +56,39 @@ class IndexControllerTest {
     @Test
     void testAssumptionFalse() {
         assumeTrue("ajay".equalsIgnoreCase("Ajay"));
+    }
+
+    @EnabledOnOs(OS.MAC)
+    @Test
+    void testOnMac() {
+        System.out.println("This is Mac OS");
+        assertEquals("Mac", "Mac", "This is Mac OS");
+    }
+
+    @EnabledOnOs(OS.WINDOWS)
+    @Test
+    void testOnWindows() {
+        System.out.println("This is Windows OS");
+        assertEquals("Windows", "Windows", "This is Windows OS");
+    }
+
+    @EnabledOnJre(JRE.JAVA_8)
+    @Test
+    void testMeOnJava8() {
+    }
+
+    @EnabledOnJre(JRE.JAVA_11)
+    @Test
+    void testMeOnJava11() {
+    }
+
+    @EnabledIfEnvironmentVariable(named = "USER", matches = "jt")
+    @Test
+    void testIfUserJT() {
+    }
+
+    @EnabledIfEnvironmentVariable(named = "USER", matches = "fred")
+    @Test
+    void testIfUserFred() {
     }
 }
