ea4b5bf900d60d8d0780b3572fb5c9c3c00c3091
ajay1236671 <kadamajay100@gmail.com>
2025-11-03 13:27:56 +0530

    custom Args

---
+++ TestingWithSpringboot/testing-java-junit5/src/test/java/guru/springframework/sfgpetclinic/CustomArgsProvider.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+
+import java.util.stream.Stream;
+
+public class CustomArgsProvider implements ArgumentsProvider {
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
+        return Stream.of(
+                Arguments.of("A", 1, 1),
+                Arguments.of("B", 1, 3),
+                Arguments.of("D", 2, 1));
+    }
+}
