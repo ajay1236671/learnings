79bc950412794b69ea0e2eff3ac9ef842e7146d6
ajay1236671 <kadamajay100@gmail.com>
2025-11-03 11:25:52 +0530

    nested tests

---
+++ TestingWithSpringboot/testing-java-junit5/src/test/java/guru/springframework/sfgpetclinic/fauxspring/ModelMapImplTest.java
@@ -0,0 +1,20 @@
+package guru.springframework.sfgpetclinic.fauxspring;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ModelMapImplTest {
+
+    @Test
+    void addAttribute() {
+    }
+
+    @Test
+    void testAddAttribute() {
+    }
+
+    @Test
+    void getMap() {
+    }
+}
