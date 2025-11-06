c9658228852b943c10a320de03cf960f1fb27c91
ajay1236671 <kadamajay100@gmail.com>
2025-11-07 01:19:35 +0530

    removed @Profile as test classes annoted with active profile component-scan were not able auto wire word producer

--- TestingWithSpringboot/tb2g-testing-spring-project/src/main/java/org/springframework/samples/petclinic/sfg/LaurelWordProducer.java
+++ TestingWithSpringboot/tb2g-testing-spring-project/src/main/java/org/springframework/samples/petclinic/sfg/LaurelWordProducer.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("laurel")
+//@Profile("laurel")
 @Primary
 public class LaurelWordProducer implements WordProducer {
 
