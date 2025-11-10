d5bbd813853dc5c4dfdb42e0db8da0f60b259a76
ajay1236671 <kadamajay100@gmail.com>
2025-11-10 16:24:16 +0530

    adding parameter

--- TestingWithSpringboot/tb2g-testing-spring-project/src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
+++ TestingWithSpringboot/tb2g-testing-spring-project/src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -1,22 +1,22 @@
 package org.springframework.samples.petclinic.web;
 
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.service.ClinicService;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-
-@SpringJUnitConfig(locations = {"classpath:spring/mvc-test-config.xml","classpath:spring/mvc-core-config.xml"})
+@SpringJUnitWebConfig(locations = {"classpath:spring/mvc-test-config.xml", "classpath:spring/mvc-core-config.xml"})
 class OwnerControllerTest {
 
     @Autowired
-    OwnerController controller;
+    OwnerController ownerController;
 
     @Autowired
     ClinicService clinicService;
@@ -25,18 +25,22 @@ class OwnerControllerTest {
 
     @BeforeEach
     void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
     }
 
     @Test
-    void initCreateFormTest() throws Exception {
+    void testFindByNameNotFound() throws Exception {
+        mockMvc.perform(get("/owners")
+                        .param("lastName", "Dont find ME!"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/findOwners"));
+    }
 
+    @Test
+    void initCreationFormTest() throws Exception {
         mockMvc.perform(get("/owners/new"))
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("owner"))
                 .andExpect(view().name("owners/createOrUpdateOwnerForm"));
-
     }
-
-
 }
