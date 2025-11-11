4ff89754b4c15565ef8c35ee0ef54ac260af1147
ajay1236671 <kadamajay100@gmail.com>
2025-11-11 11:19:34 +0530

    testing form Post with validation errors

--- TestingWithSpringboot/tb2g-testing-spring-project/src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
+++ TestingWithSpringboot/tb2g-testing-spring-project/src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -50,6 +50,7 @@ class OwnerControllerTest {
         reset(clinicService);
     }
 
+    @Test
     void testNewOwnerPostValid() throws Exception {
         mockMvc.perform(post("/owners/new")
                         .param("firstName", "John")
@@ -59,6 +60,19 @@ class OwnerControllerTest {
                         .param("telephone", "1234567890"))
                 .andExpect(status().is3xxRedirection());
 
+    }
+
+    @Test
+    void testNewOwnerPostNotValid() throws Exception {
+        mockMvc.perform(post("/owners/new")
+                        .param("firstName", "Jimmy")
+                        .param("lastName", "Buffett")
+                        .param("city", "Key West"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeHasErrors("owner"))
+                .andExpect(model().attributeHasFieldErrors("owner", "address"))
+                .andExpect(model().attributeHasFieldErrors("owner", "telephone"))
+                .andExpect(view().name("owners/createOrUpdateOwnerForm"));
     }
 
     @Test
