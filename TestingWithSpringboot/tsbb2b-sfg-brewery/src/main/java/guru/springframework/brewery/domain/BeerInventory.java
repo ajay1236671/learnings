76c5e47ddd2d1934ae91fcd04b79032a3ef55066
ajay1236671 <kadamajay100@gmail.com>
2025-11-18 17:20:31 +0530

    adding SFG Brewery Application

---
+++ TestingWithSpringboot/tsbb2b-sfg-brewery/src/main/java/guru/springframework/brewery/domain/BeerInventory.java
@@ -0,0 +1,34 @@
+package guru.springframework.brewery.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+/**
+ * Created by jt on 2019-01-26.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class BeerInventory extends BaseEntity{
+
+    @Builder
+    public BeerInventory(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate, Beer beer,
+                         Integer quantityOnHand) {
+        super(id, version, createdDate, lastModifiedDate);
+        this.beer = beer;
+        this.quantityOnHand = quantityOnHand;
+    }
+
+    @ManyToOne
+    private Beer beer;
+
+    private Integer quantityOnHand = 0;
+}
