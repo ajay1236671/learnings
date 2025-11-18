ea809277e3472119ff68a08046aa857b0eca9d7a
ajay1236671 <kadamajay100@gmail.com>
2025-11-18 16:43:51 +0530

    Testing with spring boot

---
+++ TestingWithSpringboot/tsbb2b-sfg-brewery/src/main/java/guru/springframework/brewery/TsbbSfgBreweryApplication.java
@@ -0,0 +1,15 @@
+package guru.springframework.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsbbSfgBreweryApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Starting TsbbSfgBreweryApplication");
+        SpringApplication.run(TsbbSfgBreweryApplication.class, args);
+        
+    }
+
+}
